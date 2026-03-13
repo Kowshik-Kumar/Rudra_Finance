b7918186f383176907547accfea9ce34b4feb916
Kowshik Kumar <kowshikrjk9@gmail.com>
2026-03-13 19:59:42 +0530

    Base added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Rudra_Finance/', 
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Expose to all network interfaces
